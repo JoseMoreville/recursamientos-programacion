d032c8cfc1fe2369b86e3ea32be76828c399522b
José Moreno <47700212+JoseMoreville@users.noreply.github.com>
2022-08-08 09:59:44 -0600

    Clase 08/08/2022 - Autenticacion y manejo de Base de datos MongoDB utilizando FastApi Users

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-from fastapi import FastAPI, Request, UploadFile
+from fastapi import FastAPI, Request, UploadFile, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from beanie import init_beanie
 from routes import api
 from datetime import date
+
+from app.db import User, db
+from app.schemas import UserCreate, UserRead, UserUpdate
+from app.users import auth_backend, current_active_user, fastapi_users
+
+
+
+
 
 
 
 app = FastAPI()
-app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
 app.include_router(api.router)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -28,5 +60,20 @@ async def hora(request: Request):
     return templates.TemplateResponse("index.html",
                                       {"request": request,
                                        "currentDate": currentDate
-                                        }
+                                       }
                                       )
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    await init_beanie(
+        database=db,
+        document_models=[
+            User,
+        ],
+    )
